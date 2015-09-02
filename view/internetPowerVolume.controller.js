eacdb926889d0de9e2472d369c20f2c5aca104ea
loveisnoend <loveisnoend@gmail.com>
2015-09-02 20:15:07 +0800

    change the gauge style

--- view/internetPowerVolume.controller.js
+++ view/internetPowerVolume.controller.js
@@ -359,9 +359,9 @@ sap.ui.controller("com.zhenergy.pcbi.view.internetPowerVolume", {
         function drawpie(e, data1, data2, id) {
 			var mychart = e.init(document.getElementById(id));
             var option = {
-                    tooltip : {
-                        formatter: "{a} <br/>{b} : {c}%"
-                    },
+                    // tooltip : {
+                    //     formatter: "{a} <br/>{b} : {c}%"
+                    // },
                     series : [
                         {
                             name:'业务指标',
@@ -375,19 +375,20 @@ sap.ui.controller("com.zhenergy.pcbi.view.internetPowerVolume", {
                             },
                             axisTick: {            // 坐标轴小标记
                                 splitNumber: 10,   // 每份split细分多少段
-                                length :12,        // 属性length控制线长
+                                length :0,        // 属性length控制线长
                                 lineStyle: {       // 属性lineStyle控制线条样式
                                     color: 'auto'
                                 }
                             },
                             axisLabel: {           // 坐标轴文本标签，详见axis.axisLabel
+                                show : false,
                                 textStyle: {       // 其余属性默认使用全局文本样式，详见TEXTSTYLE
-                                    color: 'auto'
+                                    color: 'green'
                                 }
                             },
                             splitLine: {           // 分隔线
                                 show: true,        // 默认显示，属性show控制显示与否
-                                length :30,         // 属性length控制线长
+                                length :10,         // 属性length控制线长
                                 lineStyle: {       // 属性lineStyle（详见lineStyle）控制线条样式
                                     color: 'auto'
                                 }
@@ -397,24 +398,27 @@ sap.ui.controller("com.zhenergy.pcbi.view.internetPowerVolume", {
                             },
                             title : {
                                 show : true,
-                                offsetCenter: [0, '-40%'],       // x, y，单位px
+                                offsetCenter: [0, '20%'],       // x, y，单位px
+                                borderWidth: 1,
                                 textStyle: {       // 其余属性默认使用全局文本样式，详见TEXTSTYLE
-                                    fontWeight: 'bolder'
+                                    fontWeight: 'bolder',
+                                    color:'green'
                                 }
                             },
                             detail : {
                                 formatter:'{value}%',
+                                offsetCenter: [0, '10%'],       // x, y，单位px
                                 textStyle: {       // 其余属性默认使用全局文本样式，详见TEXTSTYLE
-                                    color: 'auto',
-                                    fontWeight: 'bolder'
+                                    color: 'green',
+                                    fontWeight: '400',
+                                    fontSize:20
                                 }
                             },
                             data:[
-                                 { value: 50, 
-                                    name: '环比',                                
-                                    color: 'green'
+                                  { value: 40, 
+                                    name: '环比' 
                                   }
-                                ]
+                                 ]
                         }
                     ]
                 };
