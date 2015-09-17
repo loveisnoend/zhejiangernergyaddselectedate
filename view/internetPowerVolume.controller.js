609a36a2d38bd757a46ba674afaa1369fd9e75e0
loveisnoend <loveisnoend@gmail.com>
2015-09-17 22:19:18 +0800

    profit page

--- view/internetPowerVolume.controller.js
+++ view/internetPowerVolume.controller.js
@@ -34,6 +34,14 @@ sap.ui.controller("com.zhenergy.pcbi.view.internetPowerVolume", {
 			
 			function draw(e){
 			    var mychart = e.init(document.getElementById(chartDivId));
+			    document.getElementById('profitName').innerHTML = document.getElementById('powerPlantName').innerHTML;
+			    var fuelXaxisName = '';
+			    if (document.getElementById('powerPlantName').innerHTML == '集团') {
+			        fuelXaxisName = ['电厂1', '电厂2', '电厂3', '电厂4'];
+			    } else {
+			        fuelXaxisName = ['4', '5', '6', '7'];
+			    }
+			    
 			    var option = {
 			        title:{
                 	text: priceChartName,
@@ -70,7 +78,7 @@ sap.ui.controller("com.zhenergy.pcbi.view.internetPowerVolume", {
 							},
 							formatter: '{value}'
 						},
-						data: ['电厂1', '电厂2', '电厂3', '电厂4']
+						data: fuelXaxisName
                     }
                 ],
 				yAxis: [
@@ -205,16 +213,16 @@ sap.ui.controller("com.zhenergy.pcbi.view.internetPowerVolume", {
 			    // 基于准备好的dom，初始化echarts图表
                 myChart4 = ec.init(document.getElementById('powerPlantMap'));
 				var allPowerData = [
-					{name: "温州", value: 300, coal:1196820.02, coalDays:3, inputPlanTotal:"600", inputPlanValue:335, averUsePerH:11.8, averLoadRate: "65%", netPowerWPerH:6.19, costData:8580.15, costPer:0.45, otherAllCost:5646.66, otherCost:345.45, repairCost:580.9, peopleCost:3456.15, finaceCost:1000.56, depreciationCost:345.3},
-					{name: "义乌", value: 270, coal:3342340.02, coalDays:6, inputPlanTotal:"700", inputPlanValue:200, averUsePerH:23.2, averLoadRate: "36%", netPowerWPerH:3.56, costData:65324.05, costPer:1.98, otherAllCost:3452.45, otherCost:543.67, repairCost:456.87, peopleCost:2334.74, finaceCost:2345.56, depreciationCost:300.5},
-					{name: "杭州", value: 300, coal:5656774.02, coalDays:7, inputPlanTotal:"800", inputPlanValue:300, averUsePerH:34.5, averLoadRate: "45%", netPowerWPerH:4.50, costData:24543.15, costPer:4.35, otherAllCost:2343.56, otherCost:233.46, repairCost:234.89, peopleCost:8743.89, finaceCost:2783.90, depreciationCost:467.8},
-					{name: "绍兴", value: 120, coal:5456565.02, coalDays:7, inputPlanTotal:"900", inputPlanValue:332, averUsePerH:12.5, averLoadRate: "78%", netPowerWPerH:9.0, costData:54634, costPer:2.90, otherAllCost:8965.65, otherCost:345.67, repairCost:833.5, peopleCost:9876.23, finaceCost:7899.44, depreciationCost:249.7},
+				// 	{name: "温州", value: 300, coal:1196820.02, coalDays:3, inputPlanTotal:"600", inputPlanValue:335, averUsePerH:11.8, averLoadRate: "65%", netPowerWPerH:6.19, costData:8580.15, costPer:0.45, otherAllCost:5646.66, otherCost:345.45, repairCost:580.9, peopleCost:3456.15, finaceCost:1000.56, depreciationCost:345.3},
+				// 	{name: "义乌", value: 270, coal:3342340.02, coalDays:6, inputPlanTotal:"700", inputPlanValue:200, averUsePerH:23.2, averLoadRate: "36%", netPowerWPerH:3.56, costData:65324.05, costPer:1.98, otherAllCost:3452.45, otherCost:543.67, repairCost:456.87, peopleCost:2334.74, finaceCost:2345.56, depreciationCost:300.5},
+				// 	{name: "杭州", value: 300, coal:5656774.02, coalDays:7, inputPlanTotal:"800", inputPlanValue:300, averUsePerH:34.5, averLoadRate: "45%", netPowerWPerH:4.50, costData:24543.15, costPer:4.35, otherAllCost:2343.56, otherCost:233.46, repairCost:234.89, peopleCost:8743.89, finaceCost:2783.90, depreciationCost:467.8},
+				// 	{name: "绍兴", value: 120, coal:5456565.02, coalDays:7, inputPlanTotal:"900", inputPlanValue:332, averUsePerH:12.5, averLoadRate: "78%", netPowerWPerH:9.0, costData:54634, costPer:2.90, otherAllCost:8965.65, otherCost:345.67, repairCost:833.5, peopleCost:9876.23, finaceCost:7899.44, depreciationCost:249.7},
 					{name: "金华", value: 180, coal:5465324.02, coalDays:7, inputPlanTotal:"1000", inputPlanValue:767, averUsePerH:13.9, averLoadRate: "33%", netPowerWPerH:2.56, costData:32523.9, costPer:2.8, otherAllCost:1243.56, otherCost:873.56, repairCost:456.8, peopleCost:7676.56, finaceCost:9654.34, depreciationCost:783.3},
-					{name: "衢州", value: 130, coal:8356764.02, coalDays:8, inputPlanTotal:"400", inputPlanValue:129, averUsePerH:14.8, averLoadRate: "88%", netPowerWPerH:3.56, costData:34522.7, costPer:5.66, otherAllCost:6732.34, otherCost:872.73, repairCost:124.5, peopleCost:2346.76, finaceCost:5634.45, depreciationCost:965.34},
-					{name: "舟山", value: 140, coal:1258796.02, coalDays:7, inputPlanTotal:"400", inputPlanValue:356, averUsePerH:16.8, averLoadRate: "65%", netPowerWPerH:5.90, costData:13531.89, costPer:2.45, otherAllCost:2356.87, otherCost:124.55, repairCost:580.9, peopleCost:3678.34, finaceCost:3456.76, depreciationCost:876.56},
-					{name: "宁波", value: 156, coal:3737926.02, coalDays:3, inputPlanTotal:"500", inputPlanValue:246, averUsePerH:31.8, averLoadRate: "60%", netPowerWPerH:8.89, costData:749324.84, costPer:1.75, otherAllCost:7624.76, otherCost:383.67, repairCost:233.67, peopleCost:2359.48, finaceCost:8765.34, depreciationCost:986.3},
+				// 	{name: "衢州", value: 130, coal:8356764.02, coalDays:8, inputPlanTotal:"400", inputPlanValue:129, averUsePerH:14.8, averLoadRate: "88%", netPowerWPerH:3.56, costData:34522.7, costPer:5.66, otherAllCost:6732.34, otherCost:872.73, repairCost:124.5, peopleCost:2346.76, finaceCost:5634.45, depreciationCost:965.34},
+				// 	{name: "舟山", value: 140, coal:1258796.02, coalDays:7, inputPlanTotal:"400", inputPlanValue:356, averUsePerH:16.8, averLoadRate: "65%", netPowerWPerH:5.90, costData:13531.89, costPer:2.45, otherAllCost:2356.87, otherCost:124.55, repairCost:580.9, peopleCost:3678.34, finaceCost:3456.76, depreciationCost:876.56},
+				// 	{name: "宁波", value: 156, coal:3737926.02, coalDays:3, inputPlanTotal:"500", inputPlanValue:246, averUsePerH:31.8, averLoadRate: "60%", netPowerWPerH:8.89, costData:749324.84, costPer:1.75, otherAllCost:7624.76, otherCost:383.67, repairCost:233.67, peopleCost:2359.48, finaceCost:8765.34, depreciationCost:986.3},
 					{name: "台州", value: 110, coal:9467325.02, coalDays:10, inputPlanTotal:"400", inputPlanValue:267, averUsePerH:21.8, averLoadRate: "79%", netPowerWPerH:5.4, costData:135410.34, costPer:9.67, otherAllCost:3245.56, otherCost:826.33, repairCost:345.9, peopleCost:3998.45, finaceCost:3456.76, depreciationCost:456.4},
-					{name: "湖州", value: 90, coal:2568746.02, coalDays:2, inputPlanTotal:"500", inputPlanValue:300, averUsePerH:51.8, averLoadRate: "53%", netPowerWPerH:2.45, costData:25424.64, costPer:2.56, otherAllCost:9832.56, otherCost:127.67, repairCost:283.6, peopleCost:9876.44, finaceCost:3456.56, depreciationCost:234.67}
+				// 	{name: "湖州", value: 90, coal:2568746.02, coalDays:2, inputPlanTotal:"500", inputPlanValue:300, averUsePerH:51.8, averLoadRate: "53%", netPowerWPerH:2.45, costData:25424.64, costPer:2.56, otherAllCost:9832.56, otherCost:127.67, repairCost:283.6, peopleCost:9876.44, finaceCost:3456.56, depreciationCost:234.67}
 				];
 								
 				var option4 = {
@@ -294,16 +302,16 @@ sap.ui.controller("com.zhenergy.pcbi.view.internetPowerVolume", {
 								data :allPowerData
 							},
 							geoCoord: {
-								"温州":[120.65,28.01],
-								"义乌":[120.06,29.32],
-								"杭州":[120.19,30.26],
-								"绍兴":[120.58,30.01],
+								// "温州":[120.65,28.01],
+								// "义乌":[120.06,29.32],
+								// "杭州":[120.19,30.26],
+								// "绍兴":[120.58,30.01],
 								"金华":[119.64,29.12],
-								"衢州":[118.88,28.97],
-								"舟山":[122.207216,29.985295],
-								"宁波":[121.56,29.86],
+								// "衢州":[118.88,28.97],
+								// "舟山":[122.207216,29.985295],
+								// "宁波":[121.56,29.86],
 								"台州":[121.420757,28.656386],
-								"湖州":[120.1,30.86],
+								// "湖州":[120.1,30.86],
 								"上海":[3000,3000]
 							}
 						},
@@ -332,7 +340,10 @@ sap.ui.controller("com.zhenergy.pcbi.view.internetPowerVolume", {
 										label:{show:false}
 									}
 								},
-								data : [{name: "杭州", value: 300}]
+								data : [
+								    {name: "金华", value: 300},
+								    {name: "台州", value: 300}
+								    ]
 							}
 						}
 					]
@@ -340,14 +351,18 @@ sap.ui.controller("com.zhenergy.pcbi.view.internetPowerVolume", {
 	
 				myChart4.on(ecConfig.EVENT.CLICK, function (param){  
 
+                    document.getElementById('internetDetail').style.display = "none";
+                    document.getElementById('internetMain').style.display = "";
+                    
 					var mapSeries = option4.series[0];
                   
 					// 电厂名
 					document.getElementById('powerPlantName').innerHTML = mapSeries.markPoint.data[param.dataIndex].name;
 					
 					var selectedData = {name: mapSeries.markPoint.data[param.dataIndex].name, value: mapSeries.markPoint.data[param.dataIndex].inputPlanValue};
-					
-                    option4.series[1].markPoint.data[0] = selectedData;
+					option4.series[1].markPoint.data = [];
+					option4.series[1].markPoint.data[0] = selectedData;
+					option4.series[1].markPoint.data[1] = {name:'上海',value:0};
                     myChart4.setOption(option4);
 					
 					option5.series[1].markPoint.data = [{name:'上海',value:0}];
@@ -550,7 +565,8 @@ sap.ui.controller("com.zhenergy.pcbi.view.internetPowerVolume", {
 								data :allPowerData2
 							},
 							geoCoord: {
-                                "淮南":[116.73,32.80]
+                                "淮南":[116.73,32.80],
+                                "上海":[3000,3000]
 							}
 						},
 						{
@@ -573,7 +589,7 @@ sap.ui.controller("com.zhenergy.pcbi.view.internetPowerVolume", {
 										label:{show:false}
 									}
 								},
-								data : []
+								data : [{name: "淮南", value: 300}]
 							}
 						}
 					]
@@ -581,14 +597,17 @@ sap.ui.controller("com.zhenergy.pcbi.view.internetPowerVolume", {
 
 				myChart5.on(ecConfig.EVENT.CLICK, function (param){
 
+                    document.getElementById('internetDetail').style.display = "none";
+                    document.getElementById('internetMain').style.display = "";
+                    
 					var mapSeries = option5.series[0];
 
 					var selectedData = {name: mapSeries.markPoint.data[param.dataIndex].name, value: mapSeries.markPoint.data[param.dataIndex].inputPlanValue};
-
                     option5.series[1].markPoint.data[0] = selectedData;
                     myChart5.setOption(option5);
                 
-                    option4.series[1].markPoint.data = [{name:'上海',value:0}];
+                    option4.series[1].markPoint.data[0] = {name:'上海',value:0};
+                    option4.series[1].markPoint.data[1] = {name:'上海',value:0};
                     myChart4.setOption(option4);
                     
 					// 电厂名
