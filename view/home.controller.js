cdf8ed8bc01a02c727dbab44f1ca102baaf821b1
loveisnoend <loveisnoend@gmail.com>
2015-09-17 15:30:11 +0800

    slide behavior

--- view/home.controller.js
+++ view/home.controller.js
@@ -2,52 +2,84 @@ sap.ui.controller("com.zhenergy.pcbi.view.home", {
 
  /* initialize the swiper plugin*/
 	_drawSwiper: function() {
+
 	    require(
                 [
                     'js/idangerous.swiper'
                 ]
                 );
 		var mySwiper = new Swiper('.swiper-container',{
-		    
-		        onSlideClick: function(swiper){
-                  //alert('事件触发了;'+mySwiper.clickedSlideIndex);
-                  swiper.swipeTo(mySwiper.clickedSlideIndex, 500, false);
-                  
-                },
-		        initialSlide : 3,
-                slidesPerView : 3,
-                centeredSlides : true,
-                grabCursor : true,
-                paginationClickable :true,
-                // longSwipesRatio : 0.5,
-                // shortSwipes : false,
-                // followFinger : false,
-                noSwiping : true,
-                tdFlow: {
-                    rotate : 0,
-                    stretch : 100,
-                    depth : 400,
-                    modifier : 1,
-                    shadows : false
-                }
+	        onSlideClick: function(swiper){
+              swiper.swipeTo(mySwiper.clickedSlideIndex, 500, false);
+            },
+	        initialSlide : slidePageNum,
+            slidesPerView : 3,
+            centeredSlides : true,
+            grabCursor : true,
+            paginationClickable :true,
+            noSwiping : true,
+            tdFlow: {
+                rotate : 0,
+                stretch : 100,
+                depth : 400,
+                modifier : 1,
+                shadows : false
+            }
          });
+         $('.menu_bottom li').removeClass('but01 but02 but03 but04 but05 but06 nav_BI');
+         if (slidePageNum == 0){
+             mySwiper.swipeTo(0, 500, false);
+             $('#but01').addClass("but01 nav_BI");
+         }
+         if (slidePageNum == 1){
+             mySwiper.swipeTo(1, 500, false);
+             $('#but02').addClass("but02 nav_BI");
+         }
+         if (slidePageNum == 2){
+             mySwiper.swipeTo(2, 500, false);
+             $('#but03').addClass("but03 nav_BI");
+         }
+         if (slidePageNum == 3){
+             mySwiper.swipeTo(3, 500, false);
+             $('#but04').addClass("but04 nav_BI");
+         }
+         if (slidePageNum == 4){
+             mySwiper.swipeTo(4, 500, false);
+             $('#but05').addClass("but05 nav_BI");
+         }
+         if (slidePageNum == 5){
+             mySwiper.swipeTo(5, 500, false);
+             $('#but06').addClass("but03 nav_BI");
+         }
          $('#but01').click(function(){
               mySwiper.swipeTo(0, 500, false);
+              $('.menu_bottom li').removeClass('but01 but02 but03 but04 but05 but06 nav_BI');
+              $('#but01').addClass("but01 nav_BI");
          });
          $('#but02').click(function(){
               mySwiper.swipeTo(1, 500, false);
+              $('.menu_bottom li').removeClass('but01 but02 but03 but04 but05 but06 nav_BI');
+              $('#but02').addClass("but02 nav_BI");
          });
          $('#but03').click(function(){
               mySwiper.swipeTo(2, 500, false);
+              $('.menu_bottom li').removeClass('but01 but02 but03 but04 but05 but06 nav_BI');
+              $('#but03').addClass("but03 nav_BI");
          });
          $('#but04').click(function(){
               mySwiper.swipeTo(3, 500, false);
+              $('.menu_bottom li').removeClass('but01 but02 but03 but04 but05 but06 nav_BI');
+              $('#but04').addClass("but04 nav_BI");
          });
          $('#but05').click(function(){
               mySwiper.swipeTo(4, 500, false);
+              $('.menu_bottom li').removeClass('but01 but02 but03 but04 but05 but06 nav_BI');
+              $('#but05').addClass("but05 nav_BI");
          });
          $('#but06').click(function(){
               mySwiper.swipeTo(5, 500, false);
+              $('.menu_bottom li').removeClass('but01 but02 but03 but04 but05 but06 nav_BI');
+              $('#but06').addClass("but06 nav_BI");
          });
 	},
 	
