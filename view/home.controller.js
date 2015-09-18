25972352c801fd07549ccc426990475956917336
loveisnoend <loveisnoend@gmail.com>
2015-09-18 17:31:53 +0800

    top dynamic message

--- view/home.controller.js
+++ view/home.controller.js
@@ -1,6 +1,6 @@
 sap.ui.controller("com.zhenergy.pcbi.view.home", {
 
- /* initialize the swiper plugin*/
+    /* initialize the swiper plugin*/
 	_drawSwiper: function() {
 
 	    require(
@@ -28,7 +28,7 @@ sap.ui.controller("com.zhenergy.pcbi.view.home", {
          });
 	},
 	
-    loadData_top : function(){
+    _loadData_top : function(){
 	    var allenergy = null;
 	    var mom = null;
 	    var mParameters = {};
@@ -36,15 +36,15 @@ sap.ui.controller("com.zhenergy.pcbi.view.home", {
 		mParameters['success'] = jQuery.proxy(function(sRes) {
 			//设置数据
 			for (var i in sRes.results) {
-				if (sRes.results[i].KPI_ID == 'KPI_TEC_V_0000'){  //温度
+				if (sRes.results[i].KPI_ID == 'KPI_TEC_V_0000'){  //全社会用电量
 				    allenergy = sRes.results[i].KPI_VALUE;
 				}
-				if (sRes.results[i].KPI_ID == 'KPI_TEC_T_0000'){  //温度
+				if (sRes.results[i].KPI_ID == 'KPI_TEC_T_0000'){  //同比上升
 				    mom = sRes.results[i].KPI_VALUE;
 				}
 				
 			}
-			this.loadData02(mom,allenergy);
+			this._loadData02(mom,allenergy);
 		}, this);
 		mParameters['error'] = jQuery.proxy(function(eRes) {
 			alert("Get Data Error");
@@ -53,7 +53,7 @@ sap.ui.controller("com.zhenergy.pcbi.view.home", {
 	},
 	
 	// 设定全社会用电量和同比值
-	loadData02 : function(mom,allenergy){
+	_loadData02 : function(mom,allenergy){
 	    if(mom < 0){
 	        $('#mom_img').attr('src',"img/down.png");
 	        $('#mom').html("下降" + Math.abs(mom));
@@ -66,11 +66,12 @@ sap.ui.controller("com.zhenergy.pcbi.view.home", {
 	    
 	},
 	
-	loadData : function(){
-	    daytime = null;
-		weather = null;
-		temperature = null;
-	    place = null;
+	// 获取天气温度数据
+	_loadData : function(){
+	    var daytime = null;
+		var weather = null;
+		var temperature = null;
+	    var place = null;
 		var mParameters = {};
 		mParameters['async'] = true;
 	    
@@ -82,19 +83,156 @@ sap.ui.controller("com.zhenergy.pcbi.view.home", {
 				    weather = sRes.results[i].KPI_VALUE;
 				    daytime = sRes.results[i].KPI_DATE;
 				}
-				if (sRes.results[i].KPI_ID == 'KPI_TEM_H_0001'){  //温度
+				if (sRes.results[i].KPI_ID == 'KPI_TEM_H_0001'){  //天气
 				    temperature = sRes.results[i].KPI_VALUE;
 				}
-				
-			};
-			this.loadData01();
+			}
+			this._loadData01(daytime,weather,temperature,place);
 		}, this);
 		mParameters['error'] = jQuery.proxy(function(eRes) {
 			alert("Get Data Error");
 		}, this);
 	    sap.ui.getCore().getModel().read("SCREEN_JYYJ_01_V01", mParameters);
+	},
+	
+	_loadData01 : function(daytime,weather,temperature,place){
+	    var daytime01 = daytime.substring(0,4);
+	    var daytime02 = daytime.substring(4,6);
+	    var daytime03 = daytime.substring(6,8);
+	    switch(weather !== null){
+	        case weather == "W001":
+	             $('#home_weather').html("晴天");
+	             $('#home_weather_img').attr('src',"img/0001-weather-05.png");
+	             break;
+	        case weather == "W002":
+	             $('#home_weather').html("多云");
+	             $('#home_weather_img').attr('src',"img/0001-weather-04.png");
+	             break;
+	        case weather == "W003":
+	             $('#home_weather').html("阴天");
+	             $('#home_weather_img').attr('src',"img/0001-weather-01.png");
+	             break;
+	        case weather == "W004":
+	             $('#home_weather').html("雨");
+	             $('#home_weather_img').attr('src',"img/0001-weather-02.png");
+	             break;
+	        case weather == "W005":
+	             $('#home_weather').html("雷阵雨");
+	             $('#home_weather_img').attr('src',"img/0001-weather-03.png");
+	             break;
+	        case weather == "W006":
+	             $('#home_weather').html("雨夹雪");
+	             $('#home_weather_img').attr('src',"img/0001-weather-07.png");
+	             break;
+	        case weather == "W007":
+	             $('#home_weather').html("雾");
+	             $('#home_weather_img').attr('src',"img/0001-weather-08.png");
+	             break;
+	        case weather == "W008":
+	             $('#home_weather').html("风");
+	             $('#home_weather_img').attr('src',"img/0001-weather-10.png");
+	             break;
+	        case weather == "W009":
+	             $('#home_weather').html("雪");
+	             $('#home_weather_img').attr('src',"img/0001-weather-06.png");
+	             break;
+	        case weather == "W010":
+	             $('#home_weather').html("冰雹");
+	             $('#home_weather_img').attr('src',"img/0001-weather-09.png");
+	             break;
+	    }
+	    
+	    $('#home_temperature').html(temperature);
+	    $('#home_place').html(place);
+	    $('#home_daytime').html(daytime01 + "年" + daytime02 + "月" + daytime03 + "日");
+	    
+	    var d = new Date();
+	    var weekday=new Array(7);
+        weekday[0]="周日";
+        weekday[1]="周一";
+        weekday[2]="周二";
+        weekday[3]="周三";
+        weekday[4]="周四";
+        weekday[5]="周五";
+        weekday[6]="周六";
+        $('#home_Week').html(weekday[d.getDate()%7+1]);
+	},
+	
+	// 获取跑马灯的动态信息
+	_loadTopDynamicShowData : function(){
+	    
+	    // CPI环比
+	    var valueCPIhuanbi;
+	    // GDP增长率
+        var valueGDP;
+        // CPI同比
+        var valueCPItongbi;
+        // PPI同比
+        var valuePPItongbi;
+        // 制造业-同比 PMI
+        var valuePMIproduce;
+        // 非制造业-同比
+        var valuePMInonProduce;
+        // GDP总值
+        var valueGDPTotal;
+        
+		var mParameters = {};
+		mParameters['async'] = true;
+	    
+		mParameters['success'] = jQuery.proxy(function(sRes) {
+			//设置数据
+			for (var i in sRes.results) {
+				if (sRes.results[i].KPI_ID == 'KPI_ENV_H_0001'){  //CPI环比
+				    valueCPIhuanbi = sRes.results[i].KPI_VALUE;
+				}
+				if (sRes.results[i].KPI_ID == 'KPI_ENV_H_0005'){  //GDP增长率
+				    valueGDP = sRes.results[i].KPI_VALUE;
+				}
+				if (sRes.results[i].KPI_ID == 'KPI_ENV_T_0001'){  //CPI同比
+				    valueCPItongbi = sRes.results[i].KPI_VALUE;
+				}
+				if (sRes.results[i].KPI_ID == 'KPI_ENV_T_0002'){  //PPI同比
+				    valuePPItongbi = sRes.results[i].KPI_VALUE;
+				}
+				if (sRes.results[i].KPI_ID == 'KPI_ENV_T_0003'){  //制造业-同比
+				    valuePMIproduce = sRes.results[i].KPI_VALUE;
+				}
+				if (sRes.results[i].KPI_ID == 'KPI_ENV_T_0004'){  //非制造业-同比
+				    valuePMInonProduce = sRes.results[i].KPI_VALUE;
+				}
+				if (sRes.results[i].KPI_ID == 'KPI_ENV_V_0005'){  //GDP总值
+				    valueGDPTotal = sRes.results[i].KPI_VALUE;
+				}
+			}
+			this._loadData03(valueCPIhuanbi,valueGDP,valueCPItongbi,valuePPItongbi,valuePMIproduce,valuePMInonProduce,valueGDPTotal);
+		}, this);
+		mParameters['error'] = jQuery.proxy(function(eRes) {
+			alert("Get Data Error");
+		}, this);
+	    sap.ui.getCore().getModel().read("SCREEN_JYYJ_01_V01", mParameters);
+	},
+	
+	// 设定头部跑马灯信息
+	_loadData03 : function(valueCPIhuanbi,valueGDP,valueCPItongbi,valuePPItongbi,valuePMIproduce,valuePMInonProduce,valueGDPTotal){
+	    
+	    $('#valueCPIhuanbiID').html( Math.abs(valueCPIhuanbi)+"%↓");
+	    $('#valueGDPID').html( Math.abs(valueGDP)+"%↑");
+	    $('#valueCPItongbiID').html( Math.abs(valueCPItongbi)+"%↑");
+	    $('#valuePPItongbiID').html( Math.abs(valuePPItongbi)+"%↑");
+	    $('#valuePMIproduceID').html( Math.abs(valuePMIproduce)+"%↓");
+	    $('#valuePMInonProduceID').html( Math.abs(valuePMInonProduce)+"%↑");
+	    $('#valueGDPTotalID').html( Math.abs(valueGDPTotal));
 	    
 	    
+	   // if(mom < 0){
+	   //     $('#mom_img').attr('src',"img/down.png");
+	   //     $('#mom').html("下降" + Math.abs(mom));
+	   // }else if(mom >= 0){
+	   //     $('#mom_img').attr('src',"img/up.png");
+	   //     $('#mom').html("上升" + Math.abs(mom));
+	   // }
+	   // var allenergy_change = allenergy.substring(0,2);
+	   // $('#allenergy').html(allenergy_change);
 	},
 	
 /**
@@ -109,7 +247,9 @@ sap.ui.controller("com.zhenergy.pcbi.view.home", {
 			// not added the controller as delegate to avoid controller functions with similar names as the events
 			onAfterShow: jQuery.proxy(function() {
                 this._drawSwiper();
-                this.loadData_top();
+                this._loadData_top();
+                this._loadData();
+                this._loadTopDynamicShowData();
 			}, this)
 		});
 	},
