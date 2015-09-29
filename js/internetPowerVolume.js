b2df8fd1a819b7ee687808dc65d6c9b08b7c8690
loveisnoend <loveisnoend@gmail.com>
2015-09-29 21:23:43 +0800

    OK

--- js/internetPowerVolume.js
+++ js/internetPowerVolume.js
@@ -4,18 +4,21 @@ function internetPowerback() {
     document.getElementById('internetMain').style.display = "";
 }
 function priceDetailAnother(detailId) {
-	document.getElementById("internetMain").style.display = "none";
-	document.getElementById("internetDetail").style.display = "";
-	var priceChartId = "priceDetailDiv";
-	var priceChartName = '';
-	if (detailId == 'detail001') {
-	    priceChartName = '平均上网电价';
-	} else if(detailId == 'detail002'){
-	    priceChartName = '合约电价';   
-	} else if(detailId == 'detail003'){
-	    priceChartName = '直供电价';   
-	} else if(detailId == 'detail004'){
-	    priceChartName = '代替电价';   
-	}
-	internetPowerVolume.getController().loadPriceChartdetail(priceChartId, priceChartName);
+    
+    if (detailId == 'detail001') {
+    	document.getElementById("internetMain").style.display = "none";
+    	document.getElementById("internetDetail").style.display = "";
+    	var priceChartId = "priceDetailDiv";
+    	var priceChartName = '';
+    	if (detailId == 'detail001') {
+    	    priceChartName = '平均上网电价';
+    	} else if(detailId == 'detail002'){
+    	    priceChartName = '合约电价';   
+    	} else if(detailId == 'detail003'){
+    	    priceChartName = '直供电价';   
+    	} else if(detailId == 'detail004'){
+    	    priceChartName = '代替电价';   
+    	}
+    	internetPowerVolume.getController().loadPriceChartData(priceChartId, priceChartName);
+    }
 }
